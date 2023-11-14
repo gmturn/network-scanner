e2c1e436595df34d8bdba4c2d5230a21ae49a960
gmturn <turnermclean2402@gmail.com>
2023-11-13 21:18:25 -0600

    organizational updates

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from networkModels import NetworkDevice, NetworkScanner
+from src.networkModels import NetworkDevice, NetworkScanner
 
 myDevice = NetworkDevice(ipAddr="192.168.1.156", macAddr="e0:d8:c4:ef:bb:8c")
 
