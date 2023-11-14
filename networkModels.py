7dad90f219eede9b334bb9506f1cc082f303dda7
gmturn <turnermclean2402@gmail.com>
2023-11-13 20:57:10 -0600

    removed comments

--- networkModels.py
+++ networkModels.py
@@ -34,21 +34,4 @@ class NetworkScanner:
         
         for requestPacket in unansweredDevices:
             self.unansweredDevices.append(requestPacket.pdst)
-
-         
-
-
-    
-
-
-# request = scapy.ARP() 
-  
-# request.pdst = '192.168.1.1/24'
-# broadcast = scapy.Ether() 
-  
-# broadcast.dst = 'ff:ff:ff:ff:ff:ff'
-  
-# request_broadcast = broadcast / request 
-# clients = scapy.srp(request_broadcast, timeout = 1)[0] 
-# for element in clients: 
-#     print(element[1].psrc + "      " + element[1].hwsrc) 
+            
